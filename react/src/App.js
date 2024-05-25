35d863e1df04ac6f098cf13c0fdec1f28be52ece
istamosh <63596969+istamosh@users.noreply.github.com>
2024-05-25 22:35:16 +0700

    React: Introducing inline styles

--- react/src/App.js
+++ react/src/App.js
@@ -34,7 +34,6 @@ class ParentComponent extends React.Component {
       <div>
         <h1>I am the parent</h1>
         <ChildComponent />
-        {/* Change code above this line */}
       </div>
     );
   }
@@ -105,10 +104,9 @@ class TypesOfFood extends React.Component {
     return (
       <div>
         <h1>Types of Food:</h1>
-        {/* Change code below this line */}
+
         <Fruits />
         <Vegetables />
-        {/* Change code above this line */}
       </div>
     );
   }
@@ -133,9 +131,7 @@ class TypesOfFood extends React.Component {
 const CurrentDate = (props) => {
   return (
     <div>
-      {/* Change code below this line */}
       <p>The current date is: {props.date}</p>
-      {/* Change code above this line */}
     </div>
   );
 };
@@ -147,9 +143,8 @@ class Calendar extends React.Component {
     return (
       <div>
         <h3>What date is it?</h3>
-        {/* Change code below this line */}
+
         <CurrentDate date={Date()} />
-        {/* Change code above this line */}
       </div>
     );
   }
@@ -174,7 +169,7 @@ class ToDo extends React.Component {
       <div>
         <h1>To Do Lists</h1>
         <h2>Today</h2>
-        {/* Change code below this line */}
+
         <List
           tasks={[
             "wash dishes",
@@ -196,7 +191,6 @@ class ToDo extends React.Component {
             "book",
           ]}
         />
-        {/* Change code above this line */}
       </div>
     );
   }
@@ -408,7 +402,7 @@ class StatefulComponent extends React.Component {
 
 // Use State to Toggle an Element
 // toggle visibility between true and false with just one button
-class MyComponent extends React.Component {
+/* class MyComponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -441,6 +435,339 @@ class MyComponent extends React.Component {
       );
     }
   }
+} */
+
+// Write a Simple Counter
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+    // Change code below this line
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+    this.reset = this.reset.bind(this);
+    // Change code above this line
+  }
+  // Change code below this line
+  increment() {
+    this.setState((state) => ({
+      count: state.count + 1,
+    }));
+  }
+  decrement() {
+    this.setState((state) => ({
+      count: state.count - 1,
+    }));
+  }
+  reset() {
+    this.setState((state) => ({
+      count: (state.count = 0),
+    }));
+  }
+  // Change code above this line
+  render() {
+    return (
+      <div>
+        <button className="inc" onClick={this.increment}>
+          Increment!
+        </button>
+        <button className="dec" onClick={this.decrement}>
+          Decrement!
+        </button>
+        <button className="reset" onClick={this.reset}>
+          Reset
+        </button>
+        <h1>Current Count: {this.state.count}</h1>
+      </div>
+    );
+  }
 }
 
-ReactDOM.render(<MyComponent />, document.getElementById("challenge-node"));
+// Create a Controlled Input
+class ControlledInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: "",
+    };
+    // Change code below this line
+    this.handleChange = this.handleChange.bind(this);
+    // Change code above this line
+  }
+  // Change code below this line
+  handleChange(event) {
+    this.setState({
+      input: event.target.value,
+    });
+  }
+  // Change code above this line
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          value={this.state.input}
+          onChange={this.handleChange}
+        />
+
+        <h4>Controlled Input:</h4>
+        <p>{this.state.input}</p>
+      </div>
+    );
+  }
+}
+
+// Create a controlled form
+class MyForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: "",
+      submit: "",
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({
+      input: event.target.value,
+    });
+  }
+  handleSubmit(event) {
+    // Change code below this line
+    event.preventDefault();
+    this.setState((state) => ({
+      submit: state.input,
+    }));
+    // Change code above this line
+  }
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            value={this.state.input}
+            onChange={this.handleChange}
+          />
+          <button type="submit">Submit!</button>
+        </form>
+        <h1>{this.state.submit}</h1>
+      </div>
+    );
+  }
+}
+
+// Pass State as Props to Child Components
+/* class MyApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "Istamosh",
+    };
+  }
+  render() {
+    return (
+      <div>
+        <Navbar name={this.state.name} />
+      </div>
+    );
+  }
+}
+
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Hello, my name is: {this.props.name}</h1>
+      </div>
+    );
+  }
+} */
+
+// Pass a callback as props
+class MyApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: "",
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    this.setState({
+      inputValue: event.target.value,
+    });
+  }
+  render() {
+    return (
+      <div>
+        <GetInput
+          input={this.state.inputValue}
+          handleChange={this.handleChange}
+        />
+        <RenderInput input={this.state.inputValue} />
+      </div>
+    );
+  }
+}
+
+class GetInput extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h3>Get Input:</h3>
+        <input value={this.props.input} onChange={this.props.handleChange} />
+      </div>
+    );
+  }
+}
+
+class RenderInput extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h3>Input Render:</h3>
+        <p>{this.props.input}</p>
+      </div>
+    );
+  }
+}
+
+// Use the Lifecycle Method componentWillMount
+/* class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  componentWillMount() {
+    // Change code below this line
+    console.log("component is mounted!");
+    // Change code above this line
+  }
+  render() {
+    return <div />;
+  }
+} */
+
+// Use the Lifecycle Method componentDidMount
+/* class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeUsers: null,
+    };
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        activeUsers: 1273,
+      });
+    }, 2500);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Active Users: {this.state.activeUsers}</h1>
+      </div>
+    );
+  }
+} */
+
+// Add Event Listeners
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: "",
+    };
+    this.handleEnter = this.handleEnter.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+  }
+  // Change code below this line
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleKeyPress);
+  }
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleKeyPress);
+  }
+  // Change code above this line
+  handleEnter() {
+    this.setState((state) => ({
+      message: state.message + "You pressed the enter key! ",
+    }));
+  }
+  handleKeyPress(event) {
+    if (event.keyCode === 13) {
+      this.handleEnter();
+    }
+  }
+  render() {
+    return (
+      <div>
+        <h1>{this.state.message}</h1>
+      </div>
+    );
+  }
+}
+
+// Optimize Re-Renders with shouldComponentUpdate
+class OnlyEvens extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("Should I update?");
+    // Change code below this line
+    return nextProps.value % 2 === 0 ? true : false;
+    // Change code above this line
+  }
+  componentDidUpdate() {
+    console.log("Component re-rendered.");
+  }
+  render() {
+    return <h1>{this.props.value}</h1>;
+  }
+}
+
+class Controller extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 0,
+    };
+    this.addValue = this.addValue.bind(this);
+  }
+  addValue() {
+    this.setState((state) => ({
+      value: state.value + 1,
+    }));
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.addValue}>Add</button>
+        <OnlyEvens value={this.state.value} />
+      </div>
+    );
+  }
+}
+
+// Introducing Inline Styles
+class Colorful extends React.Component {
+  render() {
+    return <div>Big Red</div>;
+  }
+}
+
+ReactDOM.render(<Colorful />, document.getElementById("challenge-node"));
--- react/src/index.js
+++ react/src/index.js
@@ -5,3 +5,4 @@ import App from "./App";
 /* ReactDOM.render(<App />, document.getElementById("root")); */
 
 // redirected to App.js
+// but you still need this existence of file to make the React work
