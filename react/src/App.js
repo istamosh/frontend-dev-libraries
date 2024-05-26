262c1cd388b0b1f406949f8a0e75eac5e49d8926
istamosh <63596969+istamosh@users.noreply.github.com>
2024-05-26 16:19:07 +0700

    React: Use && for a more concise conditional

--- react/src/App.js
+++ react/src/App.js
@@ -685,7 +685,7 @@ class RenderInput extends React.Component {
 } */
 
 // Add Event Listeners
-class MyComponent extends React.Component {
+/* class MyComponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -719,7 +719,7 @@ class MyComponent extends React.Component {
       </div>
     );
   }
-}
+} */
 
 // Optimize Re-Renders with shouldComponentUpdate
 class OnlyEvens extends React.Component {
@@ -764,10 +764,150 @@ class Controller extends React.Component {
 }
 
 // Introducing Inline Styles
+/* class Colorful extends React.Component {
+  render() {
+    return <div style={{ color: "red", fontSize: 72 }}>Big Red</div>;
+  }
+} */
+
+// Add Inline Styles in React
+const styles = { color: "purple", fontSize: 40, border: "2px solid purple" };
+// Change code above this line
 class Colorful extends React.Component {
   render() {
-    return <div>Big Red</div>;
+    // Change code below this line
+    return <div style={styles}>Style Me!</div>;
+    // Change code above this line
   }
 }
 
-ReactDOM.render(<Colorful />, document.getElementById("challenge-node"));
+// Use Advanced JavaScript in React Render Method
+const inputStyle = {
+  width: 235,
+  margin: 5,
+};
+
+class MagicEightBall extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userInput: "",
+      randomIndex: "",
+    };
+    this.ask = this.ask.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  ask() {
+    if (this.state.userInput) {
+      this.setState({
+        randomIndex: Math.floor(Math.random() * 20),
+        userInput: "",
+      });
+    }
+  }
+  handleChange(event) {
+    this.setState({
+      userInput: event.target.value,
+    });
+  }
+  render() {
+    const possibleAnswers = [
+      "It is certain",
+      "It is decidedly so",
+      "Without a doubt",
+      "Yes, definitely",
+      "You may rely on it",
+      "As I see it, yes",
+      "Outlook good",
+      "Yes",
+      "Signs point to yes",
+      "Reply hazy try again",
+      "Ask again later",
+      "Better not tell you now",
+      "Cannot predict now",
+      "Concentrate and ask again",
+      "Don't count on it",
+      "My reply is no",
+      "My sources say no",
+      "Most likely",
+      "Outlook not so good",
+      "Very doubtful",
+    ];
+    const answer = possibleAnswers[this.state.randomIndex]; // Change this line
+    return (
+      <div>
+        <input
+          type="text"
+          value={this.state.userInput}
+          onChange={this.handleChange}
+          style={inputStyle}
+        />
+        <br />
+        <button onClick={this.ask}>Ask the Magic Eight Ball!</button>
+        <br />
+        <h3>Answer:</h3>
+        <p>{answer}</p>
+      </div>
+    );
+  }
+}
+
+// Render with an If-Else Condition
+/* class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: true,
+    };
+    this.toggleDisplay = this.toggleDisplay.bind(this);
+  }
+  toggleDisplay() {
+    this.setState((state) => ({
+      display: !state.display,
+    }));
+  }
+  render() {
+    // Change code below this line
+    if (this.state.display) {
+      return (
+        <div>
+          <button onClick={this.toggleDisplay}>Toggle Display</button>
+          <h1>Displayed!</h1>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <button onClick={this.toggleDisplay}>Toggle Display</button>
+        </div>
+      );
+    }
+  }
+} */
+
+// Use && for a More Concise Conditional
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: true,
+    };
+    this.toggleDisplay = this.toggleDisplay.bind(this);
+  }
+  toggleDisplay() {
+    this.setState((state) => ({
+      display: !state.display,
+    }));
+  }
+  render() {
+    // Change code below this line
+    return (
+      <div>
+        <button onClick={this.toggleDisplay}>Toggle Display</button>
+        {this.state.display && <h1>Displayed!</h1>}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<MyComponent />, document.getElementById("challenge-node"));
