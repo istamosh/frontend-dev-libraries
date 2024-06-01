f8799f41377c67a05c78302028563b14224b45f3
istamosh <63596969+istamosh@users.noreply.github.com>
2024-06-01 18:40:31 +0700

    react-redux: 20%

--- react-redux-legacy/my-app/src/react-redux-playground/index.js
+++ react-redux-legacy/my-app/src/react-redux-playground/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import "../App.css";
 
 // Getting Started with React Redux
-class DisplayMessages extends React.Component {
+/* class DisplayMessages extends React.Component {
   // Change code below this line
   constructor(props) {
     super(props);
@@ -14,6 +14,52 @@ class DisplayMessages extends React.Component {
   // Change code above this line
   render() {
     return <div />;
+  }
+} */
+
+class DisplayMessages extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      input: "",
+      messages: [],
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.submitMessage = this.submitMessage.bind(this);
+  }
+  // Add handleChange() and submitMessage() methods here
+  handleChange = (e) => {
+    this.setState({
+      input: e.target.value,
+    });
+  };
+  submitMessage = () => {
+    this.setState({
+      messages: [...this.state.messages, this.state.input],
+      input: "",
+    });
+  };
+  render() {
+    return (
+      <div>
+        <h2>Type in a new Message:</h2>
+        {/* Render an input, button, and ul below this line */}
+        <input
+          type="text"
+          value={this.state.input}
+          onChange={this.handleChange}
+        />
+        <button onClick={this.submitMessage}>Button</button>
+        <ul>
+          {this.state.messages.map((el) => (
+            <li key={el.toString()}>{el}</li>
+          ))}
+        </ul>
+        {/* Change code above this line */}
+      </div>
+    );
   }
 }
 
