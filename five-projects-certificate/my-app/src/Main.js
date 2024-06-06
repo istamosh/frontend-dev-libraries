63f5f3981dae149894d8fba9f6443e83328b90e6
istamosh <63596969+istamosh@users.noreply.github.com>
2024-06-06 16:23:08 +0700

    5cert: 2019 1:02:30

---
+++ five-projects-certificate/my-app/src/Main.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+import Tutorial from "./tutorial-phase/App";
+
+function Main() {
+  return (
+    <div>
+      <Tutorial />
+    </div>
+  );
+}
+
+export default Main;
