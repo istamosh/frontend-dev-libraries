4aafeb29cc4b64bf50dc29d962ae1db251322183
istamosh <63596969+istamosh@users.noreply.github.com>
2024-06-08 17:12:04 +0700

    5cert: trying to use async/await/promise again

--- five-projects-certificate/my-app/src/random-quotes-generator/App.js
+++ five-projects-certificate/my-app/src/random-quotes-generator/App.js
@@ -1,12 +1,27 @@
 import React from "react";
 
+fetch(
+  "https://gist.githubusercontent.com/nasrulhazim/54b659e43b1035215cd0ba1d4577ee80/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
+)
+  .then((response) => response.json())
+  .then((result) => {
+    console.log(result.quotes);
+    console.log(result.quotes.length);
+    console.log(`${result.quotes[0].quote}; ${result.quotes[0].author}`);
+  });
+
+// async function wait() {
+//   console.log(await item["quotes"]);
+// }
+// wait();
+
 const App = () => (
   <div>
     <div id="quote-box">
       <p id="text">Quote Text</p>
       <p id="author">Quote Author</p>
       <button id="new-quote">New Quote</button>
-      <a href="" id="tweet-quote">
+      <a href="" id="tweet-quote" target="_blank">
         Tweet It!
       </a>
     </div>
