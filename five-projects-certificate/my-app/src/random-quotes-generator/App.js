61b51d2a18464e8843c41528d42cc121a21ff2a6
istamosh <63596969+istamosh@users.noreply.github.com>
2024-06-09 15:30:11 +0700

    5cert: fetching data retries

--- five-projects-certificate/my-app/src/random-quotes-generator/App.js
+++ five-projects-certificate/my-app/src/random-quotes-generator/App.js
@@ -1,14 +1,75 @@
 import React from "react";
 
-fetch(
-  "https://gist.githubusercontent.com/nasrulhazim/54b659e43b1035215cd0ba1d4577ee80/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
-)
-  .then((response) => response.json())
-  .then((result) => {
-    console.log(result.quotes);
-    console.log(result.quotes.length);
-    console.log(`${result.quotes[0].quote}; ${result.quotes[0].author}`);
-  });
+/* const fetchQuotes = async () => {
+  try {
+    const fetched = await fetch(
+      "https://gist.githubusercontent.com/nasrulhazim/54b659e43b1035215cd0ba1d4577ee80/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
+    );
+    return await fetched.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+(async () => {
+  console.log(await fetchQuotes());
+})(); */
+
+const fetchQuotes = async () => {
+  let fetched;
+
+  const tryFetch = async () => {
+    fetched = await fetch(
+      "https://gist.githubusercontent.com/nasrulhazim/54b659e43b1035215cd0ba1d4577ee80/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
+    ); // Making the req
+    return await fetched.json(); // parsing the response
+  };
+
+  while (!fetched.ok) {
+    let timeOut = 5000;
+    await tryFetch();
+  }
+
+  if (fetched.ok) {
+    console.log(fetched);
+  } else {
+    while (!fetched.ok) {}
+  }
+
+  // if (fetched.ok) {
+  //   console.log(fetched.ok);
+  //   const error = new Error();
+  //   return error; // return success object
+  // } else {
+  //   console.log("elsed");
+  // }
+
+  /* const responseError = {
+    type: 'Error',
+    message: result.message || 'Something went wrong',
+    data: result.data || '',
+    code: result.code || '',
+  };
+
+  const error = new Error();
+  error.info = responseError;
+
+  return (error); */
+};
+
+(async () => {
+  console.log(await fetchQuotes());
+})();
+
+// fetch(
+//   "https://gist.githubusercontent.com/nasrulhazim/54b659e43b1035215cd0ba1d4577ee80/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
+// )
+//   .then((response) => response.json())
+//   .then((result) => {
+//     console.log(result.quotes);
+//     console.log(result.quotes.length);
+//     console.log(`${result.quotes[0].quote}; ${result.quotes[0].author}`);
+//   });
 
 // async function wait() {
 //   console.log(await item["quotes"]);
