bfecd108803800af19a82af28b71258367cc1f2d
istamosh <63596969+istamosh@users.noreply.github.com>
2024-06-10 10:39:33 +0700

    5cert: handled fetch and need working on react useState

--- five-projects-certificate/my-app/src/random-quotes-generator/App.js
+++ five-projects-certificate/my-app/src/random-quotes-generator/App.js
@@ -5,26 +5,26 @@ const fetchQuotes = async () => {
     const fetched = await fetch(
       "https://gist.githubusercontent.com/nasrulhazim/54b659e43b1035215cd0ba1d4577ee80/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
     );
-    if (!fetched.ok) throw "fetch failed!";
-    return await fetched.json();
+    if (!fetched.ok) {
+      throw new Error("Throw: fetch failed!");
+    } else {
+      return await fetched.json();
+    }
   } catch (error) {
-    console.log(error);
+    console.log("Error while fetching quotes: ", error);
     return false;
   }
 };
 
 (async () => {
   let fetch = await fetchQuotes();
-  // check if fetch is failed (false)
-  let timeOut = 5000;
-  while (!fetch) {
-    setTimeout(async () => {
-      fetch = await fetchQuotes();
-    }, timeOut);
-    timeOut += 5000;
-  }
   // do something
-  console.log(fetch);
+  if (!fetch) {
+    console.log(
+      "Fetch failed! Please check your connection and reload your browser"
+    );
+  }
+  console.log("Fetch success! ", fetch.quotes);
 })();
 
 const App = () => (
@@ -33,7 +33,7 @@ const App = () => (
       <p id="text">Quote Text</p>
       <p id="author">Quote Author</p>
       <button id="new-quote">New Quote</button>
-      <a href="" id="tweet-quote" target="_blank">
+      <a href="twitter.com/intent/tweet" id="tweet-quote" target="_blank">
         Tweet It!
       </a>
     </div>
