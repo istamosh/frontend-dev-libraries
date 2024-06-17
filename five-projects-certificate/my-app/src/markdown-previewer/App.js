7bbe3b552b6b0b36ab77a2fb53f4e1984b88153c
istamosh <63596969+istamosh@users.noreply.github.com>
2024-06-17 13:27:46 +0700

    markdown previewer: base test completed, beautifying

--- five-projects-certificate/my-app/src/markdown-previewer/App.js
+++ five-projects-certificate/my-app/src/markdown-previewer/App.js
@@ -3,7 +3,7 @@ import React from "react";
 import $ from "jquery";
 // import { connect, Provider } from "react-redux";
 // import "./styles/style.css";
-import { marked } from "https://cdnjs.cloudflare.com/ajax/libs/marked/4.3.0/lib/marked.esm.js";
+import { marked } from "https://cdnjs.cloudflare.com/ajax/libs/marked/13.0.0/lib/marked.esm.js";
 
 class Presentational extends React.Component {
   constructor(props) {
@@ -16,29 +16,47 @@ class Presentational extends React.Component {
   componentDidMount() {
     $(document).ready(function () {
       $("#main")
-        .addClass("d-flex justify-content-center align-items-center")
+        .addClass(
+          "d-flex justify-content-center align-items-center flex-column"
+        )
         .css({
-          width: "50vw",
-          height: "50vh",
           margin: "0 auto",
         });
-      $("#editor").val(
-        "# This is Heading 1\n## This is Heading 2\n### This is Heading 3\n[My Github](https://istamosh.github.io/)\n\
-          'This is Inline Code'\n\
-              ```<div>\n\
-              <div>\n\
-              <p>Hello world! This is Code Block</p>\n\
-              </div>\n\
-              </div> ```\n\
-              1. First item\n\
-              2. Second item\n\
-              3. Third item\n\
-              > This is a Blockquote\n\
-              ![This is an Image](https://cdn.freecodecamp.org/curriculum/cat-photo-app/relaxing-cat.jpg)\n\
-              **This is a Bold Text**\n\
-          "
-      );
+      // handle tab indentation textarea
+      $("#editor")
+        .on("keydown", function (e) {
+          if (e.keyCode === 9) {
+            var start = this.selectionStart;
+            var end = this.selectionEnd;
+
+            // set textarea value to: text before caret + tab + text after caret
+            this.value =
+              this.value.substring(0, start) + "\t" + this.value.substring(end);
+
+            // put caret at right position again
+            this.selectionStart = this.selectionEnd = start + 1;
+
+            // prevent focus lose
+            e.preventDefault();
+          }
+        })
+        .css({ width: "80vw", height: "20vw" });
     });
+    this.setState({
+      text: "# This is Heading 1\n## This is Heading 2\n### This is Heading 3\n[My Github](https://istamosh.github.io/) \n\n`This is Inline Code`\n ```html\n<div>\n\t<div>\n\t\t<p>Hello world! This is Code Block</p>\n\t</div>\n</div>\n```\n1. First item\n2. Second item\n3. Third item \n \n> This is a Blockquote \n \n![This is an Image](https://cdn.freecodecamp.org/curriculum/cat-photo-app/relaxing-cat.jpg) \n \n**This is a Bold Text**\n <br> And this is for line breaks <br> (br tag)",
+    });
+  }
+  // do every update
+  componentDidUpdate() {
+    // $(document).ready(function () {
+    //   // handle proper blockquote for bootstrap 5
+    //   $("#preview blockquote").addClass("blockquote");
+    //   // .css({
+    //   //   "border-left": "3px solid",
+    //   //   "padding-left": "5px",
+    //   //   // "margin-left": "25px",
+    //   // });
+    // });
   }
   handleChange(e) {
     this.setState({
@@ -46,10 +64,10 @@ class Presentational extends React.Component {
     });
   }
   render() {
-    const parse = marked.parse(this.state.text);
+    // const parse = marked.parse(this.state.text).replace(/&gt;+/g, ">");
     return (
       <>
-        <Html record={this.handleChange} html={parse} />
+        <Html text={this.state.text} record={this.handleChange} />
       </>
     );
   }
@@ -63,8 +81,15 @@ const Html = (props) => {
         name="editor1"
         id="editor"
         onChange={props.record}
+        value={props.text}
       ></textarea>
-      <div id="preview" dangerouslySetInnerHTML={{ __html: props.html }}></div>
+      <div
+        id="preview"
+        dangerouslySetInnerHTML={{
+          __html: marked(props.text, { breaks: true }),
+          // .replace(/&gt;+/g, ">"),
+        }}
+      ></div>
     </div>
   );
 };
