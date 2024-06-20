b3701e00005779b8235b6d44ca20f33b846f2d64
istamosh <63596969+istamosh@users.noreply.github.com>
2024-06-20 21:52:26 +0700

    markdown previewer: styling completed

--- five-projects-certificate/my-app/src/markdown-previewer/App.js
+++ five-projects-certificate/my-app/src/markdown-previewer/App.js
@@ -19,34 +19,48 @@ class Presentational extends React.Component {
       $("#main").addClass(
         "d-flex justify-content-center align-items-center flex-column"
       );
+      // handle tab indentation textarea
+      $("#editor")
+        .on("keydown", function (e) {
+          if (e.keyCode === 9) {
+            var start = this.selectionStart;
+            var end = this.selectionEnd;
+
+            // set textarea value to: text before caret + tab + text after caret
+            this.value =
+              this.value.substring(0, start) + "\t" + this.value.substring(end);
+
+            // put caret at right position again
+            this.selectionStart = this.selectionEnd = start + 1;
+
+            // prevent focus lose
+            e.preventDefault();
+          }
+        })
+        .addClass("form-control font-monospace lh-sm text-nowrap shadow");
+      $(".editor-area label").addClass(
+        "form-label m-0 ms-2 d-block text-truncate text-white lead fs-6"
+      );
+      $(".editor-area h1").addClass("text-nowrap text-center");
+      $("#preview").addClass("border border-1 rounded p-2 bg-body shadow");
     });
-    // handle tab indentation textarea
-    $("#editor").on("keydown", function (e) {
-      if (e.keyCode === 9) {
-        var start = this.selectionStart;
-        var end = this.selectionEnd;
 
-        // set textarea value to: text before caret + tab + text after caret
-        this.value =
-          this.value.substring(0, start) + "\t" + this.value.substring(end);
-
-        // put caret at right position again
-        this.selectionStart = this.selectionEnd = start + 1;
-
-        // prevent focus lose
-        e.preventDefault();
-      }
-    });
     this.setState({
-      text: "# This is Heading 1\n## This is Heading 2\n### This is Heading 3\n| Syntax      | Description | Test Text     |\n| :---        |    :----:   |          ---: |\n| Header      | Title       | Here's this   |\n| Paragraph   | Text        | And more      |\n\n[My Github](https://istamosh.github.io/) \n\n`This is Inline Code`\n ```html\n<div>\n\t<div>\n\t\t<p>Hello world! This is Code Block</p>\n\t</div>\n</div>\n```\n1. First item\n2. Second item\n3. Third item \n \n> This is a Blockquote \n> and this is another blockquote! \n \n![This is an Image of a kitten](https://cdn.freecodecamp.org/curriculum/cat-photo-app/relaxing-cat.jpg) \n \n**This is a Bold Text**\n <br> And this is for line breaks <br> `<br> tag`",
+      text: "# This is Heading 1\n## This is Heading 2\n### This is Heading 3\n| Syntax      | Description | Test Text     |\n| :---        |    :----:   |          ---: |\n| Header      | Title       | Here's this   |\n| Paragraph   | Text        | And more      |\n\n[My Github](https://istamosh.github.io/) \n\n`This is an Inline Code`\n ```html\n<div>\n\t<div>\n\t\t<p>Hello world! This is Code Block</p>\n\t</div>\n</div>\n```\n1. First item\n2. Second item\n3. Third item \n \n> This is a Blockquote \n> and this is another blockquote! \n \n![This is an Image of a kitten](https://cdn.freecodecamp.org/curriculum/cat-photo-app/relaxing-cat.jpg) \n \n**This is a Bold Text**\n <br> And this is for line breaks <br> `<br> tag`",
     });
   }
   // do every update
   componentDidUpdate() {
     Prism.highlightAll();
+
     $("#preview table").addClass(
       "table table-dark table-striped table-hover table-sm"
     );
+    $("#preview img").addClass("mw-100");
+    $("#preview h1").addClass("border-bottom border-5 rounded-bottom");
+    $("#preview h2").addClass("border-bottom border-3 rounded-bottom");
+    $("#preview h3").addClass("border-bottom border-1 rounded-bottom");
+    $("#preview p a").attr("target", "_blank");
   }
   handleChange(e) {
     this.setState({
@@ -54,7 +68,6 @@ class Presentational extends React.Component {
     });
   }
   render() {
-    // const parse = marked.parse(this.state.text).replace(/&gt;+/g, ">");
     return (
       <>
         <Html text={this.state.text} record={this.handleChange} />
@@ -66,13 +79,20 @@ class Presentational extends React.Component {
 const Html = (props) => {
   return (
     <div id="main">
-      <textarea
-        spellCheck="false"
-        name="editor1"
-        id="editor"
-        onChange={props.record}
-        value={props.text}
-      ></textarea>
+      <div className="editor-area">
+        <h1>.md Previewer</h1>
+        <label for="editor">
+          Write your Markdown text here <span className="arrow">&#8628;</span>
+        </label>
+        <textarea
+          spellCheck="false"
+          name="editor1"
+          id="editor"
+          onChange={props.record}
+          value={props.text}
+          style={{ resize: "none" }}
+        ></textarea>
+      </div>
       <div
         id="preview"
         dangerouslySetInnerHTML={{
