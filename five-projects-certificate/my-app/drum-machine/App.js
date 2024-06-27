178b3a4f873327699317370b19f1661b79f0309f
istamosh <63596969+istamosh@users.noreply.github.com>
2024-06-27 21:23:07 +0700

    Drum machine: building the pads

--- five-projects-certificate/my-app/drum-machine/App.js
+++ five-projects-certificate/my-app/drum-machine/App.js
@@ -1,7 +1,18 @@
 const DrumMachine = () => {
   return (
     <>
-      <div id="drum-machine"></div>
+      <div id="drum-machine">
+        <div id="display"></div>
+        <div class='drum-pad' id="drum-pad-1">Q<audio class='clip' src=""></audio> </div>
+        <div class='drum-pad' id="drum-pad-2">W<audio class='clip' src=""></audio> </div>
+        <div class='drum-pad' id="drum-pad-3">E<audio class='clip' src=""></audio> </div>
+        <div class='drum-pad' id="drum-pad-4">A<audio class='clip' src=""></audio> </div>
+        <div class='drum-pad' id="drum-pad-5">S<audio class='clip' src=""></audio> </div>
+        <div class='drum-pad' id="drum-pad-6">D<audio class='clip' src=""></audio> </div>
+        <div class='drum-pad' id="drum-pad-7">Z<audio class='clip' src=""></audio> </div>
+        <div class='drum-pad' id="drum-pad-8">X<audio class='clip' src=""></audio> </div>
+        <div class='drum-pad' id="drum-pad-9">C<audio class='clip' src=""></audio> </div>
+      </div>
     </>
   );
 };
