acfbb8df4a82c708c34b46b3ecfd3738b2799b5a
istamosh <63596969+istamosh@users.noreply.github.com>
2024-07-04 21:19:40 +0700

    Drum pad: 7/8 completed

--- five-projects-certificate/my-app/src/drum-machine/App.js
+++ five-projects-certificate/my-app/src/drum-machine/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import $ from "jquery";
 
 const DrumPad = (props) => (
@@ -11,13 +11,21 @@ const DrumPad = (props) => (
   //     new Audio(props.audio).play();
   //   }}
   // />
-  <div className="drum-pad" id={props.id}>
+  <div
+    className="drum-pad"
+    id={props.id}
+    onClick={() => {
+      $(`#${props.string}`)[0].play();
+    }}
+  >
     <audio className="clip" src={props.audio} id={props.string}></audio>
     {props.string}
   </div>
 );
 
 const DrumMachine = () => {
+  const [text, setText] = useState("");
+
   let audioSamples = [
     { link: "Heater-1.mp3", key: "Q" },
     { link: "Heater-2.mp3", key: "W" },
@@ -43,30 +51,55 @@ const DrumMachine = () => {
     );
   });
 
-  // const play = (arg) => {
-  //   document.querySelector("audio").play();
-  // };
+  const handleKeyboard = ({ key }) => {
+    const keypress = key.toUpperCase();
+    const audio = document.getElementById(keypress);
+
+    if (audio) {
+      audio.pause();
+      audio.play();
+      setText(audio.src.substring(57, audio.src.length - 4));
+    }
+  };
 
   const mounted = useRef();
   useEffect(() => {
     if (!mounted.current) {
-      $(document).ready(function () {
-        $(".drum-pad").each((i, obj) => {
-          console.log(obj);
-        });
-      });
+      // $(document).ready(() => {
+      //   $(".drum-pad").each((i, obj) => {
+      //     obj.addEventListener("click", (e) => {
+      //       e.preventDefault();
+      //       const hehe = obj.querySelector("audio");
+      //       var isPlaying =
+      //         hehe.currentTime > 0 &&
+      //         !hehe.paused &&
+      //         !hehe.ended &&
+      //         hehe.readyState > hehe.HAVE_CURRENT_DATA;
+
+      //       if (!isPlaying) {
+      //         hehe.play();
+      //       }
+      //     });
+      //   });
+      // });
+      document.addEventListener("keydown", handleKeyboard);
       mounted.current = true;
+      return () => {
+        document.removeEventListener("keydown", handleKeyboard);
+      };
     } else {
+      document.addEventListener("keydown", handleKeyboard);
+      return () => {
+        document.removeEventListener("keydown", handleKeyboard);
+      };
     }
   });
 
   return (
-    <>
-      <div id="drum-machine">
-        <div id="display"></div>
-        {drumPads}
-      </div>
-    </>
+    <div id="drum-machine">
+      <div id="display">{text}</div>
+      {drumPads}
+    </div>
   );
 };
 
