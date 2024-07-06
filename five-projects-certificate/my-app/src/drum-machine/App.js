c7e82d1a7f166b5514abdf57f4713a800be07c54
istamosh <63596969+istamosh@users.noreply.github.com>
2024-07-06 20:06:14 +0700

    Drum machine: 8/8, need styling

--- five-projects-certificate/my-app/src/drum-machine/App.js
+++ five-projects-certificate/my-app/src/drum-machine/App.js
@@ -2,15 +2,7 @@ import React, { useEffect, useRef, useState } from "react";
 import $ from "jquery";
 
 const DrumPad = (props) => (
-  <div
-    className="drum-pad"
-    id={props.id}
-    // onClick={(e) => {
-    //   $(`#${props.string}`)[0].play();
-    //   console.log(e.target.id);
-    // }}
-    onClick={props.trigger}
-  >
+  <div className="drum-pad" id={props.id} onClick={props.trigger}>
     <audio className="clip" src={props.audio} id={props.string}></audio>
     {props.string}
   </div>
@@ -18,56 +10,6 @@ const DrumPad = (props) => (
 
 const DrumMachine = () => {
   const [text, setText] = useState("");
-
-  const audioSamples = [
-    { link: "Heater-1.mp3", key: "Q" },
-    { link: "Heater-2.mp3", key: "W" },
-    { link: "Heater-3.mp3", key: "E" },
-    { link: "Heater-4_1.mp3", key: "A" },
-    { link: "Heater-6.mp3", key: "S" },
-    { link: "Dsc_Oh.mp3", key: "D" },
-    { link: "Kick_n_Hat.mp3", key: "Z" },
-    { link: "RP4_KICK_1.mp3", key: "X" },
-    { link: "Cev_H2.mp3", key: "C" },
-  ];
-
-  const drumPads = [];
-  audioSamples.forEach((val) => {
-    drumPads.push(
-      <DrumPad
-        key={val.link.substring(0, val.link.length - 4)}
-        id={val.link.substring(0, val.link.length - 4)}
-        audio={`https://cdn.freecodecamp.org/testable-projects-fcc/audio/${val.link}`}
-        string={val.key}
-        trigger={hitDrum}
-      />
-    );
-  });
-
-  function hitDrum({ key, target }) {
-    const keypress = key || "click!";
-    // function catchUndefined(fn, defaultVal = "undefined.") {
-    //   try {
-    //     return fn();
-    //   } catch (e) {
-    //     return defaultVal;
-    //   }
-    // }
-    // const audio = document.getElementById(keypress);
-
-    // if (audio) {
-    //   // audio.pause();
-    //   audio.play();
-    //   setText(audio.src.substring(57, audio.src.length - 4));
-    // }
-    // console.log(
-    //   catchUndefined(() => {
-    //     key.toUpperCase();
-    //   })
-    // );
-    console.log(`keyboard: ${keypress.toUpperCase()}`);
-    console.log(`click: ${target.id}`);
-  }
 
   const mounted = useRef();
   useEffect(() => {
@@ -83,6 +25,39 @@ const DrumMachine = () => {
         document.removeEventListener("keydown", hitDrum);
       };
     }
+  });
+
+  const audioSamples = [
+    { link: "Heater-1.mp3", key: "Q" },
+    { link: "Heater-2.mp3", key: "W" },
+    { link: "Heater-3.mp3", key: "E" },
+    { link: "Heater-4_1.mp3", key: "A" },
+    { link: "Heater-6.mp3", key: "S" },
+    { link: "Dsc_Oh.mp3", key: "D" },
+    { link: "Kick_n_Hat.mp3", key: "Z" },
+    { link: "RP4_KICK_1.mp3", key: "X" },
+    { link: "Cev_H2.mp3", key: "C" },
+  ];
+
+  const hitDrum = ({ key, target }) => {
+    const audio = $(`#${key ? key.toUpperCase() : target.innerText}`)[0];
+    if (audio) {
+      audio.play();
+      setText(audio.src.substring(57, audio.src.length - 4));
+    }
+  };
+
+  const drumPads = [];
+  audioSamples.forEach((val) => {
+    drumPads.push(
+      <DrumPad
+        key={val.link.substring(0, val.link.length - 4)}
+        id={val.link.substring(0, val.link.length - 4)}
+        audio={`https://cdn.freecodecamp.org/testable-projects-fcc/audio/${val.link}`}
+        string={val.key}
+        trigger={hitDrum}
+      />
+    );
   });
 
   return (
