e24eba0e49ca8c74f8742420eea240b5a18812e6
istamosh <63596969+istamosh@users.noreply.github.com>
2024-07-07 21:39:09 +0700

    drum machine: alternate play manipulation

--- five-projects-certificate/my-app/src/drum-machine/App.js
+++ five-projects-certificate/my-app/src/drum-machine/App.js
@@ -15,6 +15,17 @@ const DrumMachine = () => {
   useEffect(() => {
     if (!mounted.current) {
       document.addEventListener("keydown", hitDrum);
+
+      // warm up the audios
+      const audios = $("audio");
+      Object.keys(audios)
+        .splice(0, audios.length)
+        .forEach((el) => {
+          audios[el].preload = "auto";
+          audios[el].load();
+          audios[el].muted = true;
+        });
+
       mounted.current = true;
       return () => {
         document.removeEventListener("keydown", hitDrum);
@@ -28,22 +39,31 @@ const DrumMachine = () => {
   });
 
   const audioSamples = [
-    { link: "Heater-1.mp3", key: "Q" },
-    { link: "Heater-2.mp3", key: "W" },
-    { link: "Heater-3.mp3", key: "E" },
-    { link: "Heater-4_1.mp3", key: "A" },
-    { link: "Heater-6.mp3", key: "S" },
-    { link: "Dsc_Oh.mp3", key: "D" },
-    { link: "Kick_n_Hat.mp3", key: "Z" },
-    { link: "RP4_KICK_1.mp3", key: "X" },
-    { link: "Cev_H2.mp3", key: "C" },
+    { link: "Heater-1", key: "Q" },
+    { link: "Heater-2", key: "W" },
+    { link: "Heater-3", key: "E" },
+    { link: "Heater-4_1", key: "A" },
+    { link: "Heater-6", key: "S" },
+    { link: "Dsc_Oh", key: "D" },
+    { link: "Kick_n_Hat", key: "Z" },
+    { link: "RP4_KICK_1", key: "X" },
+    { link: "Cev_H2", key: "C" },
   ];
 
   const hitDrum = ({ key, target }) => {
     const audio = $(`#${key ? key.toUpperCase() : target.innerText}`)[0];
     if (audio) {
       audio.play();
+      // instantiating the audio to prevent error
+      const audioInstance = new Audio(audio.src);
+      audioInstance.currentTime = 0;
+      audioInstance.play();
       setText(audio.src.substring(57, audio.src.length - 4));
+
+      setTimeout(() => {
+        audioInstance.pause();
+        audioInstance.src = "";
+      }, 1000);
     }
   };
 
@@ -51,9 +71,9 @@ const DrumMachine = () => {
   audioSamples.forEach((val) => {
     drumPads.push(
       <DrumPad
-        key={val.link.substring(0, val.link.length - 4)}
-        id={val.link.substring(0, val.link.length - 4)}
-        audio={`https://cdn.freecodecamp.org/testable-projects-fcc/audio/${val.link}`}
+        key={val.link.substring(0, val.link.length)}
+        id={val.link.substring(0, val.link.length)}
+        audio={`https://cdn.freecodecamp.org/testable-projects-fcc/audio/${val.link}.mp3`}
         string={val.key}
         trigger={hitDrum}
       />
