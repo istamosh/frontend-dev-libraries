175c52e89a10e23026a10bb3c36c78af4fd2b10c
istamosh <63596969+istamosh@users.noreply.github.com>
2024-07-16 23:01:25 +0700

    Calculator: trying to remove trailing comma

--- five-projects-certificate/my-app/src/calculator/App.js
+++ five-projects-certificate/my-app/src/calculator/App.js
@@ -60,6 +60,39 @@ const Engine = () => {
     }
   });
 
+  const cleanValue = (params) => {
+    // split the numbers and operators into an array
+    let separatedSegments = params.split(/([+\-*/])/);
+
+    const transformString = (str) => {
+      // check if it's the operator
+      if (/[+\-/*]/.test(str)) {
+        return str;
+      }
+
+      let firstCommaIndex = str.indexOf(".") + 1;
+      let substringBefore = str.substring(0, firstCommaIndex);
+      // store trailing char after comma, remove all commas, and remove trailing zeroes until end
+      let substringAfter = str
+        .substring(firstCommaIndex)
+        .replace(/\./g, "")
+        .replace(/0+$/, "");
+      // console.log(substringBefore + ", " + substringAfter);
+      // concat the processed strings, if the 2nd half is empty then remove the first comma of 1st string
+      let concatenated =
+        substringAfter !== ""
+          ? substringBefore + substringAfter
+          : substringBefore + "0";
+      return concatenated.startsWith(".") ? "0" + concatenated : concatenated;
+    };
+
+    // process segments using above function, then join, then accept last operator if it have consecutive operators
+    return separatedSegments
+      .map((el) => transformString(el))
+      .join("")
+      .replace(/([+\-/*]){2,}/g, (match) => match.slice(-1));
+  };
+
   const buttonPress = ({ key, target }) => {
     let input;
     if (key) {
@@ -84,16 +117,9 @@ const Engine = () => {
             : "-" + prevValue
         );
         break;
-      case ".":
-        setResult((prevValue) =>
-          prevValue.slice(-1) !== "." ? prevValue + "." : prevValue
-        );
-        break;
       case "=":
-        setResult((prevValue) => {
-          const filteredComma = prevValue.replace(/\.+/g, ".");
-          return evaluate(filteredComma);
-        });
+        // setResult((prevValue) => parseFloat(evaluate(cleanValue(prevValue))));
+        setResult((prevValue) => evaluate(prevValue));
         break;
       case "":
         break;
