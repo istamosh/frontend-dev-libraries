a2a9279873fe2239e7e2e89f037c53f95f00f0a8
istamosh <63596969+istamosh@users.noreply.github.com>
2024-07-23 21:42:40 +0700

    pomodoro: useState init

--- five-projects-certificate/my-app/src/Main.js
+++ five-projects-certificate/my-app/src/Main.js
@@ -5,7 +5,8 @@ import "./App.css";
 // import RandomQuotes from "./random-quotes-generator/App";
 // import MarkdownPreviewer from "./markdown-previewer/App";
 // import DrumMachine from "./drum-machine/App";
-import Calculator from "./calculator/App";
+// import Calculator from "./calculator/App";
+import PomodoroTimer from "./pomodoro-timer/App";
 
 function Main() {
   return (
@@ -14,7 +15,8 @@ function Main() {
       {/* <RandomQuotes /> */}
       {/* <MarkdownPreviewer /> */}
       {/* <DrumMachine /> */}
-      <Calculator />
+      {/* <Calculator /> */}
+      <PomodoroTimer />
     </>
   );
 }
--- five-projects-certificate/my-app/src/pomodoro-timer/App.js
+++ five-projects-certificate/my-app/src/pomodoro-timer/App.js
@@ -1,1 +1,31 @@
+import React, { useState, useRef, useEffect } from "react";
+import $ from "jquery";
 import "./styles/style.css";
+
+const Pomodoro = () => {
+  const [timer, setTimer] = useState("0");
+  const [session, setSession] = useState("25");
+
+  const mounted = useRef();
+  useEffect(() => {
+    if (!mounted.current) {
+      $(function () {
+        $("#app").addClass(
+          "d-flex justify-content-center flex-column align-items-center vh-100"
+        );
+      });
+
+      mounted.current = true;
+      return () => {};
+    } else {
+      return () => {};
+    }
+  });
+
+  return (
+    <>
+      <h1>Pomodoro Timer</h1>
+    </>
+  );
+};
+export default Pomodoro;
