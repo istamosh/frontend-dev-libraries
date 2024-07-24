3399b284c2e069b88fa53c603508b324fa6a36cd
istamosh <63596969+istamosh@users.noreply.github.com>
2024-07-24 20:09:25 +0700

    pomodoro: basic layout

--- five-projects-certificate/my-app/src/pomodoro-timer/App.js
+++ five-projects-certificate/my-app/src/pomodoro-timer/App.js
@@ -4,6 +4,7 @@ import "./styles/style.css";
 
 const Pomodoro = () => {
   const [timer, setTimer] = useState("0");
+  const [breakTime, setBreakTime] = useState("5");
   const [session, setSession] = useState("25");
 
   const mounted = useRef();
@@ -25,6 +26,38 @@ const Pomodoro = () => {
   return (
     <>
       <h1>Pomodoro Timer</h1>
+      <span for="" id="break-label">
+        Break Length
+      </span>
+      <button id="break-decrement">{"<"}</button>
+      <button id="break-increment">{">"}</button>
+      <input
+        type="range"
+        min="1"
+        max="60"
+        name="break"
+        id="break-length"
+        value={breakTime}
+      />
+      <span for="" id="session-label">
+        Session Length
+      </span>
+      <button id="session-decrement">{"<"}</button>
+      <button id="session-increment">{">"}</button>
+      <input
+        type="range"
+        min="1"
+        max="60"
+        name="session"
+        id="session-length"
+        value={session}
+      />
+      <span for="" id="timer-label">
+        Session
+      </span>
+      <p id="time-left">mm:ss</p>
+      <button id="start_stop">Start / Stop</button>
+      <button id="reset">Reset</button>
     </>
   );
 };
